8c6f7d483c7b206cb93867cde4cb7bf632b0e3be
Wontae <bbrick@Wontae-3.local>
2023-08-24 11:00:58 +0900

    [Model] Keyword 모델에 keyword 추가

--- app/models/keyword.py
+++ app/models/keyword.py
@@ -2,8 +2,9 @@ from app.db_connector import db
 
 class Keyword(db.Model): 
     id = db.Column(db.Integer, primary_key=True)
+    keyword = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False)
     deleted = db.Column(db.Boolean, default=False, nullable=False)
 
     def __repr__(self):
-        return f'{self.id}번 Keyword: {self.name}'
+        return f'{self.id}번 Keyword: {self.keyword}, (name): {self.name}'
