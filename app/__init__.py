c156159d30fb639880afda9314e942767f65f3ce
Wontae <bbrick@Wontae-3.local>
2023-08-25 09:53:21 +0900

    [CrawlerService] 최초시작은 date parameter를 줍니다

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,6 @@ from flask_restful import Api
 from apscheduler.schedulers.background import BackgroundScheduler
 from app.routes import bp
 from app.db_connector import db
-# from app.controllers.news import NewsController, NewsListController
-# from app.controllers.keyword import KeywordController, KeywordListController
 from app.controllers import *
 from config import Config
 
@@ -15,8 +13,9 @@ crawlerService = CrawlerService()
 
 scheduler = BackgroundScheduler(daemon=True, timezone='Asia/Seoul')
 scheduler.add_job(crawlerService.readyAllCrawlers, 'date', run_date=datetime.datetime.now() + datetime.timedelta(seconds=1))
-scheduler.add_job(crawlerService.runAllCrawlers, 'interval', seconds=60)
-scheduler.start()
+scheduler.add_job(crawlerService.readyAllCrawlers, 'interval', seconds=300)
+scheduler.add_job(crawlerService.runAllCrawlers, 'interval', seconds=30)
+# scheduler.start()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -31,4 +30,5 @@ def create_app(config_class=Config):
     api.add_resource(KeywordController, '/keyword/<id>')
 
     app.register_blueprint(bp)
+    # crawlerService.readyAllCrawlers()
     return app
