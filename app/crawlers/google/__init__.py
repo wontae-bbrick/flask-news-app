25072dfe5a64387ec7d6f55cf39e293ae1f60e94
Wontae <bbrick@Wontae-3.local>
2023-08-25 14:50:17 +0900

    [GoogleNewsCrawler] title css selector 잘못되어서 안들어오는거 수정

--- app/crawlers/google/__init__.py
+++ app/crawlers/google/__init__.py
@@ -7,14 +7,16 @@ class GoogleNewsCrawler(NewsCrawler):
         super(GoogleNewsCrawler, self).__init__(keyword)
         self.base_url = 'https://news.google.com/search?hl=ko&gl=KR&ceid=KR%3Ako&q='
         self.platform = '구글뉴스'
+        # 이 매핑이 지금 잘못되었음
+        # article.MQsxIb
         self.target_csstag_map = {
-            'press': '#yDmH0d > c-wiz > div > div.FVeGwb.CVnAc.Haq2Hf.bWfURe > div.ajwQHc.BL5WZb.RELBvb > div > main > c-wiz > div.lBwEZb.BL5WZb.GndZbb > div:nth-child(1) > div > div > article:nth-child(1) > div.wsLqz.RD0gLb > div > a',
-            'datetime': '#yDmH0d > c-wiz > div > div.FVeGwb.CVnAc.Haq2Hf.bWfURe > div.ajwQHc.BL5WZb.RELBvb > div > main > c-wiz > div.lBwEZb.BL5WZb.GndZbb > div:nth-child(1) > div > div > article:nth-child(1) > div.QmrVtf.RD0gLb.kybdz > div > time',
-            'title': '#yDmH0d > c-wiz > div > div.FVeGwb.CVnAc.Haq2Hf.bWfURe > div.ajwQHc.BL5WZb.RELBvb > div > main > c-wiz > div.lBwEZb.BL5WZb.GndZbb > div:nth-child(1) > div > div > article:nth-child(1) > h3 > a',
-            'url': '#yDmH0d > c-wiz > div > div.FVeGwb.CVnAc.Haq2Hf.bWfURe > div.ajwQHc.BL5WZb.RELBvb > div > main > c-wiz > div.lBwEZb.BL5WZb.GndZbb > div:nth-child(1) > div > div > article:nth-child(1) > h3 > a',
+            'press': 'article.MQsxIb a.wEwyrc',
+            'datetime': 'article.MQsxIb time.WW6dff',
+            'title': 'article.MQsxIb h3 a',
+            'url': 'article.MQsxIb a.VDXfz',
         }
         self.target_content_map['platform'] = '구글뉴스'
-
+        #yDmH0d > c-wiz:nth-child(30) > div > div.FVeGwb.CVnAc.Haq2Hf.bWfURe > div.ajwQHc.BL5WZb.RELBvb > div > main > c-wiz > div.lBwEZb.BL5WZb.GndZbb > div:nth-child(99) > div > article
     def unwrap_htmltag(self, target, htmltag):
         unwrapped = ''
         if target == 'url':
@@ -22,23 +24,9 @@ class GoogleNewsCrawler(NewsCrawler):
             unwrapped = 'https://news.google.com/'+unwrapped[1:]
         elif target == 'press':
             unwrapped = htmltag.text
-            unwrapped = unwrapped[:-6] if '언론사 선정' in unwrapped else unwrapped       
         elif target == 'datetime':
-            unwrapped = htmltag.text
-            number_pattern = "\d+"
-            number = int(re.findall(number_pattern, unwrapped)[0])
-            d = None
-            try:
-                if unwrapped[-3] == '간':
-                    d = datetime.today() - timedelta(hours=number)
-                elif unwrapped[-3] == '분':
-                    d = datetime.today() - timedelta(minutes=number)
-                else: 
-                    d = datetime.today() - timedelta(days=number)
-            except:
-                d = datetime.today() - timedelta(days=1)
-        
-            unwrapped = str(d)
+            unwrapped = htmltag['datetime']
+            unwrapped = " ".join(unwrapped[:-1].split('T'))
         else:
             unwrapped = htmltag.text
         return unwrapped
